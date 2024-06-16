5bbc5bc8f28a58917bb4c22872bc2a1030252a2e
Jose Diaz <jose@exoreaction.com>
2024-06-16 12:37:17 -0500

    native demo

---
+++ demo-native/solutions/expense-function/src/main/java/pe/joedayz/training/ExpenseFunctions.java
@@ -0,0 +1,23 @@
+package pe.joedayz.training;
+
+import java.util.List;
+
+import jakarta.inject.Inject;
+
+import io.quarkus.funqy.Funq;
+
+public class ExpenseFunctions {
+
+    @Inject
+    ExpenseRepository repository;
+
+    @Funq
+    public List<Expense> expenses() {
+        return repository.all();
+    }
+
+    @Funq
+    public void createExpense( Expense expense ) {
+        repository.add( expense );
+    }
+}
---
+++ demo-native/solutions/expense-function/src/test/java/pe/joedayz/training/NativeExpenseResourceIT.java
@@ -0,0 +1,9 @@
+package pe.joedayz.training;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExpenseResourceIT extends ExpenseResourceTest {
+
+    // Execute the same tests but in native mode.
+}
