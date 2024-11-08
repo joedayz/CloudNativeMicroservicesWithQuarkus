b1d3e2b0832b9308b54db872c254ee95b2a07251
Jose Diaz <jose@exoreaction.com>
2024-11-08 15:59:12 -0500

    solucion config

--- develop-config/src/main/java/org/acme/ExpenseValidator.java
+++ develop-config/src/main/java/org/acme/ExpenseValidator.java
@@ -8,23 +8,20 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 @ApplicationScoped
 public class ExpenseValidator {
 
-    private static final boolean DEBUG_ENABLED = true;
 
-    private static final int RANGE_HIGH = 1000;
-
-    private static final int RANGE_LOW = 250;
+    @Inject
+    ExpenseConfiguration config;
 
     public void debugRanges() {
-        System.out.println("Range - High: " + RANGE_HIGH);
-        System.out.println("Range - Low: " + RANGE_LOW);
+        config.debugMessage().ifPresent(System.out::println);
     }
 
     public boolean isValidAmount(int amount) {
-        if (DEBUG_ENABLED) {
+        if (config.debugEnabled()) {
             debugRanges();
         }
 
-        return amount >= RANGE_LOW && amount <= RANGE_HIGH;
+        return amount >= config.rangeLow() && amount <= config.rangeHigh();
     }
 }
 
